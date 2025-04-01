785a23f2bc1f99d99aa989a665b339d2ecb52e33
sanja_r_q <ersainsanjar02.06@gmail.com>
2025-04-01 22:59:27 +0500

    Создание проекта

---
+++ src/main/java/com/example/bitlabproject/BitlabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bitlabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlabProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitlabProjectApplication.class, args);
+    }
+
+}
