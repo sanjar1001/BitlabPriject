e593fbbff3cbe71ad754a98c91e880fc7d8dabda
sanja_r_q <ersainsanjar02.06@gmail.com>
2025-04-02 09:05:35 +0500

    Создал сущность Course

---
+++ src/main/java/com/example/bitlabproject/entity/Course.java
@@ -0,0 +1,38 @@
+package com.example.bitlabproject.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "Courses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "createdTime")
+    private LocalDateTime createdTime;
+
+    @Column(name = "updatedTime")
+    private LocalDateTime updatedTime;
+
+}
